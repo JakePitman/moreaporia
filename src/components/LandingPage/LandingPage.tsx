e704054107c3fcb6954df46ab5871906dbc7d019
Jay Pitman <jakeseanp@gmail.com>
2022-02-08 23:08:56 +1100

    Add heading/subheadings to landing page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -7,7 +7,9 @@ const LandingPage = () => {
     <div className={styles.container}>
       <div className={styles.navBar}></div>
       <div className={styles.midSection}>
-        <h1>Hello Landing Page</h1>
+        <h1 className={styles.mainHeading}>Jake Pitman</h1>
+        <h2 className={styles.subheading1}>Web & mobile engineer</h2>
+        <h2 className={styles.subheading2}>Front-end specialist</h2>
       </div>
       <div className={styles.footer}></div>
     </div>
