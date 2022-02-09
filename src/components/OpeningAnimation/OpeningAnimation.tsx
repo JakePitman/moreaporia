40d7a3c4e9c825a0508464abf256491c27942fd3
Jay Pitman <jakeseanp@gmail.com>
2022-02-10 00:01:53 +1100

    Extract opening animation variants

--- src/components/OpeningAnimation/OpeningAnimation.tsx
+++ src/components/OpeningAnimation/OpeningAnimation.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { motion, useAnimation } from "framer-motion";
+
 import styles from "./OpeningAnimation.module.scss";
+import {
+  mainSquareVariants,
+  logoTextVariants,
+  borderedSquareVariants,
+  fadedSquareVariants,
+} from "./variants";
 
 type Props = {
   setCompleted: () => void;
@@ -8,155 +15,6 @@ type Props = {
 
 const OpeningAnimation = ({ setCompleted }: Props) => {
   const controls = useAnimation();
-
-  const mainSquareVariants = {
-    hidden: {
-      width: "0px",
-      height: "0px",
-      borderRadius: "50%",
-      rotate: -180,
-      opacity: 0,
-      transition: {},
-      y: 0,
-    },
-    expanding: {
-      borderRadius: 0,
-      opacity: 1,
-      rotate: 0,
-      width: "100px",
-      height: "100px",
-      transition: {
-        duration: 0.4,
-        delay: 1,
-        type: "spring",
-      },
-    },
-    aboutToClose: {
-      width: "110px",
-      height: "110px",
-      transition: {
-        delay: 0.8,
-        duration: 0.15,
-      },
-    },
-    closing: {
-      width: "5px",
-      height: "5px",
-      rotate: 500,
-      borderRadius: "50%",
-      transition: {
-        duration: 0.3,
-      },
-    },
-    goingFullScreen1: {
-      width: "500vw",
-      borderRadius: 0,
-      transition: {
-        duration: 0.6,
-        delay: 0.2,
-      },
-    },
-    goingFullScreen2: {
-      height: "500vh",
-      transition: {
-        duration: 0.6,
-      },
-    },
-  };
-
-  const borderedSquareVariants = {
-    hidden: (custom: { startOffset: number; finishOffset: number }) => ({
-      opacity: 0,
-      y: custom.startOffset,
-      x: custom.startOffset,
-      width: "0px",
-      height: "0px",
-    }),
-    expanding: (custom: { startOffset: number; finishOffset: number }) => ({
-      opacity: 1,
-      x: custom.finishOffset,
-      y: custom.finishOffset,
-      width: "60px",
-      height: "60px",
-      transition: {
-        duration: 0.4,
-        delay: 1.2,
-        type: "spring",
-      },
-    }),
-    aboutToClose: {
-      width: "65px",
-      height: "65px",
-      transition: {
-        duration: 0.15,
-        delay: 0.8,
-      },
-    },
-    closing: {
-      opacity: 0,
-      width: "0px",
-      height: "0px",
-      x: 0,
-      y: 0,
-      rotate: 180,
-      transition: {
-        duration: 0.3,
-      },
-    },
-  };
-
-  const fadedSquareVariants = {
-    hidden: (custom: { startOffset: number; finishOffset: number }) => ({
-      opacity: 0,
-      y: -custom.startOffset,
-      x: custom.startOffset,
-      width: "0px",
-      height: "0px",
-    }),
-    expanding: (custom: { startOffset: number; finishOffset: number }) => ({
-      opacity: 0.5,
-      y: -custom.finishOffset,
-      x: custom.finishOffset,
-      width: "100px",
-      height: "100px",
-      transition: {
-        duration: 0.4,
-        delay: 1.4,
-        type: "spring",
-      },
-    }),
-    aboutToClose: {
-      width: "110px",
-      height: "110px",
-      transition: {
-        duration: 0.15,
-        delay: 0.8,
-      },
-    },
-    closing: {
-      opacity: 0,
-      width: "0px",
-      height: "0px",
-      x: 0,
-      y: 0,
-      rotate: -180,
-      transition: {
-        duration: 0.3,
-      },
-    },
-  };
-
-  const logoTextVariants = {
-    hidden: {
-      opacity: 0,
-    },
-    expanding: {
-      opacity: 1,
-    },
-    closing: {
-      opacity: 0,
-    },
-  };
 
   const sequence = async () => {
     await controls.start("expanding");
---
+++ src/components/OpeningAnimation/variants.ts
@@ -0,0 +1,148 @@
+export const mainSquareVariants = {
+  hidden: {
+    width: "0px",
+    height: "0px",
+    borderRadius: "50%",
+    rotate: -180,
+    opacity: 0,
+    transition: {},
+    y: 0,
+  },
+  expanding: {
+    borderRadius: 0,
+    opacity: 1,
+    rotate: 0,
+    width: "100px",
+    height: "100px",
+    transition: {
+      duration: 0.4,
+      delay: 1,
+      type: "spring",
+    },
+  },
+  aboutToClose: {
+    width: "110px",
+    height: "110px",
+    transition: {
+      delay: 0.8,
+      duration: 0.15,
+    },
+  },
+  closing: {
+    width: "5px",
+    height: "5px",
+    rotate: 500,
+    borderRadius: "50%",
+    transition: {
+      duration: 0.3,
+    },
+  },
+  goingFullScreen1: {
+    width: "500vw",
+    borderRadius: 0,
+    transition: {
+      duration: 0.6,
+      delay: 0.2,
+    },
+  },
+  goingFullScreen2: {
+    height: "500vh",
+    transition: {
+      duration: 0.6,
+    },
+  },
+};
+
+export const borderedSquareVariants = {
+  hidden: (custom: { startOffset: number; finishOffset: number }) => ({
+    opacity: 0,
+    y: custom.startOffset,
+    x: custom.startOffset,
+    width: "0px",
+    height: "0px",
+  }),
+  expanding: (custom: { startOffset: number; finishOffset: number }) => ({
+    opacity: 1,
+    x: custom.finishOffset,
+    y: custom.finishOffset,
+    width: "60px",
+    height: "60px",
+    transition: {
+      duration: 0.4,
+      delay: 1.2,
+      type: "spring",
+    },
+  }),
+  aboutToClose: {
+    width: "65px",
+    height: "65px",
+    transition: {
+      duration: 0.15,
+      delay: 0.8,
+    },
+  },
+  closing: {
+    opacity: 0,
+    width: "0px",
+    height: "0px",
+    x: 0,
+    y: 0,
+    rotate: 180,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
+
+export const fadedSquareVariants = {
+  hidden: (custom: { startOffset: number; finishOffset: number }) => ({
+    opacity: 0,
+    y: -custom.startOffset,
+    x: custom.startOffset,
+    width: "0px",
+    height: "0px",
+  }),
+  expanding: (custom: { startOffset: number; finishOffset: number }) => ({
+    opacity: 0.5,
+    y: -custom.finishOffset,
+    x: custom.finishOffset,
+    width: "100px",
+    height: "100px",
+    transition: {
+      duration: 0.4,
+      delay: 1.4,
+      type: "spring",
+    },
+  }),
+  aboutToClose: {
+    width: "110px",
+    height: "110px",
+    transition: {
+      duration: 0.15,
+      delay: 0.8,
+    },
+  },
+  closing: {
+    opacity: 0,
+    width: "0px",
+    height: "0px",
+    x: 0,
+    y: 0,
+    rotate: -180,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
+
+export const logoTextVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  expanding: {
+    opacity: 1,
+  },
+  closing: {
+    opacity: 0,
+  },
+};
