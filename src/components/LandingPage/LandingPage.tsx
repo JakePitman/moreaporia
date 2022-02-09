09f4fd129c86589a1b87530fb2eabdf1b925c2a7
Jay Pitman <jakeseanp@gmail.com>
2022-02-09 19:21:11 +1100

    Add yellow to first letter of each list item

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -6,15 +6,23 @@ const LandingPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.navBar}>
-        <li className={styles.listItem}>About</li>
-        <li className={styles.listItem}>Projects</li>
-        <li className={styles.listItem}>Contact</li>
+        <li className={styles.listItem}>
+          <p className={styles.listItemFirstLetter}>A</p>bout
+        </li>
+        <li className={styles.listItem}>
+          <p className={styles.listItemFirstLetter}>P</p>rojects
+        </li>
+        <li className={styles.listItem}>
+          <p className={styles.listItemFirstLetter}>C</p>ontact
+        </li>
       </div>
+
       <div className={styles.midSection}>
         <h1 className={styles.mainHeading}>Jake Pitman</h1>
         <h2 className={styles.subheading1}>Web & mobile engineer</h2>
         <h2 className={styles.subheading2}>Front-end specialist</h2>
       </div>
+
       <div className={styles.footer}></div>
     </div>
   );
