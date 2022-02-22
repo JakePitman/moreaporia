4bead11b7fbf802e563931203b6cd570161f0a31
Jay Pitman <jakeseanp@gmail.com>
2022-02-22 23:22:36 +1100

    Animate angled backgrounds

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { motion } from "framer-motion";
+
 import styles from "./AboutPage.module.scss";
+import {
+  lowerLeftBackgroundVariants,
+  lowerRightBackgroundVariants,
+} from "./aboutPageVariants";
 
 const AboutPage = () => {
   return (
     <div className={styles.container}>
       <h1 style={{ color: "white" }}>About me</h1>
-      <div className={styles.lowerLeftBackground} />
-      <div className={styles.lowerRightBackground} />
+      <motion.div
+        variants={lowerLeftBackgroundVariants}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, margin: "200px" }}
+        className={styles.lowerLeftBackground}
+      />
+      <motion.div
+        variants={lowerRightBackgroundVariants}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, margin: "200px" }}
+        className={styles.lowerRightBackground}
+      />
     </div>
   );
 };
