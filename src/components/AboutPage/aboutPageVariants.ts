f33b1fd3b3c708f28f5499b48375da31fbd8a6aa
Jay Pitman <jakeseanp@gmail.com>
2022-02-22 23:36:06 +1100

    Add slight delay and fix position of angled backgrounds

---
+++ src/components/AboutPage/aboutPageVariants.ts
@@ -0,0 +1,31 @@
+export const lowerLeftBackgroundVariants = {
+  hidden: {
+    rotate: 0,
+    bottom: "-60vh",
+    transition: {},
+  },
+
+  visible: {
+    rotate: 30,
+    bottom: "-50vh",
+    transition: {
+      delay: 0.1,
+    },
+  },
+};
+
+export const lowerRightBackgroundVariants = {
+  hidden: {
+    rotate: 0,
+    bottom: "-70vh",
+    transition: {},
+  },
+
+  visible: {
+    rotate: -35,
+    bottom: "-60vh",
+    transition: {
+      delay: 0.1,
+    },
+  },
+};
