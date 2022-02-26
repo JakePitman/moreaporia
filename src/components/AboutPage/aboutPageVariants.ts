c57e0aca9bbe5db6d4f9c50e787b180c89aea284
Jay Pitman <jakeseanp@gmail.com>
2022-02-26 15:09:31 +1100

    Standardise visible/hidden heights & increase lower right width

--- src/components/AboutPage/aboutPageVariants.ts
+++ src/components/AboutPage/aboutPageVariants.ts
@@ -1,30 +1,36 @@
+const lowerBackgroundVisibleHeight = "-90vh";
+const lowerBackgroundHiddenHeight = "-110vh";
+
 export const lowerLeftBackgroundVariants = {
   hidden: {
     rotate: 0,
-    bottom: "-110vh",
+    bottom: lowerBackgroundHiddenHeight,
     transition: {},
   },
 
   visible: {
     rotate: 30,
-    bottom: "-90vh",
+    bottom: lowerBackgroundVisibleHeight,
     transition: {
       delay: 0.3,
     },
   },
   workActive: {
     rotate: 40,
+    bottom: lowerBackgroundVisibleHeight,
     transition: {
       delay: 0.1,
     },
   },
   jakeActive: {
     rotate: 30,
+    bottom: lowerBackgroundVisibleHeight,
     transition: {
       delay: 0.1,
     },
   },
   techActive: {
+    bottom: lowerBackgroundVisibleHeight,
     rotate: 20,
   },
 };
@@ -32,23 +38,26 @@ export const lowerLeftBackgroundVariants = {
 export const lowerRightBackgroundVariants = {
   hidden: {
     rotate: 0,
-    bottom: "-70vh",
+    bottom: lowerBackgroundHiddenHeight,
     transition: {},
   },
   visible: {
     rotate: -30,
-    bottom: "-60vh",
+    bottom: lowerBackgroundVisibleHeight,
     transition: {
       delay: 0.2,
     },
   },
   workActive: {
+    bottom: lowerBackgroundVisibleHeight,
     rotate: -10,
   },
   jakeActive: {
+    bottom: lowerBackgroundVisibleHeight,
     rotate: -30,
   },
   techActive: {
+    bottom: lowerBackgroundVisibleHeight,
     rotate: -40,
     transition: {
       delay: 0.1,
