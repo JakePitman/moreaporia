c8599a3dbc1e9591c0c3ccb05c4ecef1c2a5432f
Jay Pitman <jakeseanp@gmail.com>
2022-02-26 14:54:22 +1100

    Animate lower corners when selected nav changes

--- src/components/AboutPage/aboutPageVariants.ts
+++ src/components/AboutPage/aboutPageVariants.ts
@@ -12,6 +12,21 @@ export const lowerLeftBackgroundVariants = {
       delay: 0.3,
     },
   },
+  workActive: {
+    rotate: 40,
+    transition: {
+      delay: 0.1,
+    },
+  },
+  jakeActive: {
+    rotate: 30,
+    transition: {
+      delay: 0.1,
+    },
+  },
+  techActive: {
+    rotate: 20,
+  },
 };
 
 export const lowerRightBackgroundVariants = {
@@ -20,12 +35,23 @@ export const lowerRightBackgroundVariants = {
     bottom: "-70vh",
     transition: {},
   },
-
   visible: {
     rotate: -30,
     bottom: "-60vh",
     transition: {
       delay: 0.2,
+    },
+  },
+  workActive: {
+    rotate: -10,
+  },
+  jakeActive: {
+    rotate: -30,
+  },
+  techActive: {
+    rotate: -40,
+    transition: {
+      delay: 0.1,
     },
   },
 };
