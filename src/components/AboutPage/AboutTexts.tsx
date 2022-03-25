d24cc1ba39e43e8f3695ca150a56d865872d3d77
Jay Pitman <jakeseanp@gmail.com>
2022-03-25 21:41:33 +1100

    Style areas for workText landscape/portrait

--- src/components/AboutPage/AboutTexts.tsx
+++ src/components/AboutPage/AboutTexts.tsx
@@ -3,15 +3,25 @@ import { motion } from "framer-motion";
 
 import AboutText from "./AboutText";
 import styles from "./AboutTexts.module.scss";
+import useWindowDimensions from "../../shared/useWindowDimensions";
 
 type Props = {
   selectedNavOption: "jake" | "work" | "tech" | null;
 };
 
 const AboutTexts = ({ selectedNavOption }: Props) => {
+  const { width, height } = useWindowDimensions();
+  const isLandscape = width > height;
+
   if (selectedNavOption === "work") {
     return (
-      <motion.div className={styles.workTextContainer}>
+      <motion.div
+        className={
+          isLandscape
+            ? styles.workTextContainerLandscape
+            : styles.workTextContainerPortrait
+        }
+      >
         <AboutText text="My cool work" visible={selectedNavOption === "work"} />
       </motion.div>
     );
@@ -20,13 +30,25 @@ const AboutTexts = ({ selectedNavOption }: Props) => {
   if (selectedNavOption === "jake") {
     return (
       <>
-        <motion.div className={styles.jakeTextContainerLeft}>
+        <motion.div
+          className={
+            isLandscape
+              ? styles.jakeTextContainerLeftLandscape
+              : styles.jakeTextContainerLeftPortrait
+          }
+        >
           <AboutText
             text="My cool jake"
             visible={selectedNavOption === "jake"}
           />
         </motion.div>
-        <motion.div className={styles.jakeTextContainerRight}>
+        <motion.div
+          className={
+            isLandscape
+              ? styles.jakeTextContainerRightLandscape
+              : styles.jakeTextContainerRightPortrait
+          }
+        >
           <AboutText
             text="My cool jake 2"
             visible={selectedNavOption === "jake"}
@@ -38,7 +60,13 @@ const AboutTexts = ({ selectedNavOption }: Props) => {
 
   if (selectedNavOption === "tech") {
     return (
-      <motion.div className={styles.techTextContainer}>
+      <motion.div
+        className={
+          isLandscape
+            ? styles.techTextContainerLandscape
+            : styles.techTextContainerPortrait
+        }
+      >
         <AboutText text="My cool tech" visible={selectedNavOption === "tech"} />
       </motion.div>
     );
