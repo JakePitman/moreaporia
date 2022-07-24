8899671edc0f0789c3fb569bfe8cc9ed9ca980cb
Jake Pitman <jakeseanp@gmail.com>
2022-07-24 16:03:51 +1000

    Cleanup import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import "./App.scss";
 import LandingPage from "./components/LandingPage";
 import AboutPage from "./components/AboutPage";
