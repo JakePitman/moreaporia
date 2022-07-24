5c71631ce02ac796152dd75c26ab412d3968d6d5
Jake Pitman <jakeseanp@gmail.com>
2022-07-24 18:44:18 +1000

    Don't display AppNavigation on load screen

--- src/components/AppNavigation/AppNavigation.tsx
+++ src/components/AppNavigation/AppNavigation.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useLocation,
-} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const AppNavigation = () => {
+  if (useLocation().pathname === "/") return <></>;
   return (
     <ul>
       <Link to="/home">Home</Link>
