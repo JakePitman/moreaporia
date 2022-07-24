a0a053d68526334d75443f203887e569ba20b29d
Jake Pitman <jakeseanp@gmail.com>
2022-07-24 13:33:16 +1000

    Add basic router implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import LandingPage from "./components/LandingPage";
 import AboutPage from "./components/AboutPage";
 import OpeningAnimation from "./components/OpeningAnimation";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 // cache first AboutPage image before AboutPage loads
 import kayakImage from "./components/AboutPage/pictures/kayak.png";
@@ -17,22 +18,29 @@ function App() {
     useState(false);
 
   return (
-    <div className="App">
-      {openingAnimationCompleted ? (
-        <>
-          <LandingPage
-            scrollToAboutPage={() => {
-              aboutPageRef.current?.scrollIntoView({ behavior: "smooth" });
-            }}
+    <Router>
+      <div className="App">
+        {openingAnimationCompleted ? (
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <LandingPage
+                  scrollToAboutPage={() =>
+                    aboutPageRef.current?.scrollIntoView({ behavior: "smooth" })
+                  }
+                />
+              }
+            />
+            <Route path="/about" element={<AboutPage ref={aboutPageRef} />} />
+          </Routes>
+        ) : (
+          <OpeningAnimation
+            setCompleted={() => setOpeningAnimationCompleted(true)}
           />
-          <AboutPage ref={aboutPageRef} />
-        </>
-      ) : (
-        <OpeningAnimation
-          setCompleted={() => setOpeningAnimationCompleted(true)}
-        />
-      )}
-    </div>
+        )}
+      </div>
+    </Router>
   );
 }
 
