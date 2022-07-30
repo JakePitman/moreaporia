c20817f3609b2d3e34591c90dbdaa8685ae2dcf9
Jake Pitman <jakeseanp@gmail.com>
2022-07-30 21:52:08 +1000

    Make flip cards work for all states

--- src/components/AboutPage/flipCardVariants.ts
+++ src/components/AboutPage/flipCardVariants.ts
@@ -1,57 +1,77 @@
+const leftToRightMoveDefaults = {
+  right: 150,
+};
+const leftToRightActiveDefaults = ({ delay }: { delay: number }) => ({
+  transform: "rotateY(180deg)",
+  left: 0,
+  transition: {
+    duration: 0.5,
+    delay: delay,
+  },
+});
 export const cardVariantsLeftToRight = {
-  jakeActiveMove: {
-    right: 150,
-  },
-  jakeActiveExpand: ({ delay }: { delay: number }) => ({
-    transform: "rotateY(180deg)",
-    left: 0,
-    transition: {
-      duration: 0.5,
-      delay: delay,
-    },
-  }),
+  jakeActiveMove: leftToRightMoveDefaults,
+  techActiveMove: leftToRightMoveDefaults,
+  workActiveMove: leftToRightMoveDefaults,
+  workActiveExpand: leftToRightActiveDefaults,
+  jakeActiveExpand: leftToRightActiveDefaults,
+  techActiveExpand: leftToRightActiveDefaults,
 };
 
+const RightToLeftMoveDefaults = {
+  right: -150,
+};
+const RightToLeftActiveDefaults = ({ delay }: { delay: number }) => ({
+  transform: "rotateY(180deg)",
+  left: 0,
+  transition: {
+    duration: 0.5,
+    delay: delay,
+  },
+});
 export const cardVariantsRightToLeft = {
-  jakeActiveMove: {
-    right: -150,
-  },
-  jakeActiveExpand: ({ delay }: { delay: number }) => ({
-    transform: "rotateY(180deg)",
-    left: 0,
-    transition: {
-      duration: 0.5,
-      delay: delay,
-    },
-  }),
+  jakeActiveMove: RightToLeftMoveDefaults,
+  techActiveMove: RightToLeftMoveDefaults,
+  workActiveMove: RightToLeftMoveDefaults,
+  workActiveExpand: RightToLeftActiveDefaults,
+  jakeActiveExpand: RightToLeftActiveDefaults,
+  techActiveExpand: RightToLeftActiveDefaults,
 };
 
+const TopToBottomMoveDefaults = {
+  bottom: 150,
+};
+const TopToBottomActiveDefaults = ({ delay }: { delay: number }) => ({
+  transform: "rotateX(180deg)",
+  top: 0,
+  transition: {
+    duration: 0.5,
+    delay: delay,
+  },
+});
 export const cardVariantsTopToBottom = {
-  jakeActiveMove: {
-    bottom: 150,
-  },
-  jakeActiveExpand: ({ delay }: { delay: number }) => ({
-    transform: "rotateX(180deg)",
-    top: 0,
-    transition: {
-      duration: 0.5,
-      delay: delay,
-    },
-  }),
+  jakeActiveMove: TopToBottomMoveDefaults,
+  techActiveMove: TopToBottomMoveDefaults,
+  workActiveMove: TopToBottomMoveDefaults,
+  workActiveExpand: TopToBottomActiveDefaults,
+  jakeActiveExpand: TopToBottomActiveDefaults,
+  techActiveExpand: TopToBottomActiveDefaults,
 };
 
-export const cardVariantsBottomToTop = {
-  jakeActiveMove: {
-    bottom: -150,
+const BottomToTopMoveDefaults = {
+  bottom: -150,
+};
+const BottomToTopActiveDefaults = ({ delay }: { delay: number }) => ({
+  transform: "rotateX(180deg)",
+  top: 0,
+  transition: {
+    duration: 0.5,
+    delay: delay,
   },
-  jakeActiveExpand: ({ delay }: { delay: number }) => ({
-    transform: "rotateX(180deg)",
-    top: 0,
-    transition: {
-      duration: 0.5,
-      delay: delay,
-    },
-  }),
+});
+export const cardVariantsBottomToTop = {
+  jakeActiveMove: BottomToTopMoveDefaults,
+  jakeActiveExpand: BottomToTopActiveDefaults,
 };
 export const tagVariants = {
   jakeActiveMove: {
