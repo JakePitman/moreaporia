3425cbc083d6649333c550e465c0346aee3aa4f9
Jake Pitman <jakeseanp@gmail.com>
2022-07-31 14:25:57 +1000

    Add small flip cards & fill out rows with dummy cards

--- src/components/AboutPage/AboutWorkContent.tsx
+++ src/components/AboutPage/AboutWorkContent.tsx
@@ -10,7 +10,9 @@ import colors from "../../shared/_colors.module.scss";
 import BrowserContext from "../../shared/browserContext";
 const { gradient1, gradient3, gradient5 } = colors;
 
-const LandscapeLayout = () => {
+const AboutJakeContent = () => {
+  const { isLandscape } = useContext(BrowserContext);
+
   return (
     <div className={styles.rowsContainer}>
       <div className={styles.row}>
@@ -29,9 +31,23 @@ const LandscapeLayout = () => {
             <p className={styles.contentCardText}>First: Tooling</p>
           </FlipCard>
         </div>
-      </div>
 
-      <div className={styles.row}>
+        <div
+          className={styles.flipCardContainerLandscapeSmall}
+          style={{ transform: `rotate(${10}deg)` }}
+        >
+          <FlipCard
+            title="My cool card"
+            delay={0}
+            rotate={10}
+            tagRotation={5}
+            backgroundGradients={[gradient1, gradient3]}
+            variants={cardVariantsLeftToRight}
+          >
+            <p className={styles.contentCardText}>First: Tooling</p>
+          </FlipCard>
+        </div>
+
         <div
           className={styles.flipCardContainerLandscape}
           style={{ transform: `rotate(${10}deg)` }}
@@ -48,17 +64,56 @@ const LandscapeLayout = () => {
           </FlipCard>
         </div>
       </div>
+
+      <div className={styles.row}>
+        <div
+          className={styles.flipCardContainerLandscapeSmall}
+          style={{ transform: `rotate(${10}deg)` }}
+        >
+          <FlipCard
+            title="My cool card"
+            delay={0}
+            rotate={10}
+            tagRotation={5}
+            backgroundGradients={[gradient1, gradient3]}
+            variants={cardVariantsLeftToRight}
+          >
+            <p className={styles.contentCardText}>First: Tooling</p>
+          </FlipCard>
+        </div>
+        <div
+          className={styles.flipCardContainerLandscape}
+          style={{ transform: `rotate(${10}deg)` }}
+        >
+          <FlipCard
+            title="My cool card"
+            delay={0}
+            rotate={10}
+            tagRotation={5}
+            backgroundGradients={[gradient1, gradient3]}
+            variants={cardVariantsLeftToRight}
+          >
+            <p className={styles.contentCardText}>First: Tooling</p>
+          </FlipCard>
+        </div>
+        <div
+          className={styles.flipCardContainerLandscapeSmall}
+          style={{ transform: `rotate(${10}deg)` }}
+        >
+          <FlipCard
+            title="My cool card"
+            delay={0}
+            rotate={10}
+            tagRotation={5}
+            backgroundGradients={[gradient1, gradient3]}
+            variants={cardVariantsLeftToRight}
+          >
+            <p className={styles.contentCardText}>First: Tooling</p>
+          </FlipCard>
+        </div>
+      </div>
     </div>
   );
-};
-const PortraitLayout = () => {
-  return <p>Hello</p>;
-};
-
-const AboutJakeContent = () => {
-  const { isLandscape } = useContext(BrowserContext);
-
-  return isLandscape ? <LandscapeLayout /> : <PortraitLayout />;
 };
 
 export default AboutJakeContent;
