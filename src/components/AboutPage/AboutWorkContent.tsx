2e39e875cbd3066be588961231a18f963367e095
Jake Pitman <jakeseanp@gmail.com>
2022-07-31 16:42:15 +1000

    Render landscape from mapped data

--- src/components/AboutPage/AboutWorkContent.tsx
+++ src/components/AboutPage/AboutWorkContent.tsx
@@ -10,108 +10,86 @@ import colors from "../../shared/_colors.module.scss";
 import BrowserContext from "../../shared/browserContext";
 const { gradient1, gradient3, gradient5 } = colors;
 
+const flipCardData = {
+  landscape: [
+    [
+      {
+        rotate: 3,
+        delay: 0,
+        children: <p className={styles.contentCardText}>First: Tooling!</p>,
+      },
+      {
+        rotate: -3,
+        delay: 0.2,
+        children: <p className={styles.contentCardText}>2</p>,
+        small: true,
+      },
+      {
+        rotate: 4,
+        delay: 0.4,
+        children: (
+          <p className={styles.contentCardText}>Third: Something better!</p>
+        ),
+      },
+    ],
+    [
+      {
+        rotate: -3,
+        delay: 0.1,
+        children: <p className={styles.contentCardText}>1</p>,
+        small: true,
+      },
+      {
+        rotate: 2,
+        delay: 0.3,
+        children: (
+          <p className={styles.contentCardText}>Second: Something else!</p>
+        ),
+      },
+      {
+        rotate: -2,
+        delay: 0.5,
+        children: <p className={styles.contentCardText}>3</p>,
+        small: true,
+      },
+    ],
+  ],
+};
+
 const AboutJakeContent = () => {
   const { isLandscape } = useContext(BrowserContext);
 
   return (
     <div className={styles.rowsContainer}>
-      <div className={styles.row}>
-        <div
-          className={styles.flipCardContainerLandscape}
-          style={{ transform: `rotate(${10}deg)` }}
-        >
-          <FlipCard
-            title="My cool card"
-            delay={0}
-            rotate={10}
-            tagRotation={5}
-            backgroundGradients={[gradient1, gradient3]}
-            variants={cardVariantsLeftToRight}
-          >
-            <p className={styles.contentCardText}>First: Tooling</p>
-          </FlipCard>
+      {flipCardData.landscape.map((row, rowNumber) => (
+        <div className={styles.row}>
+          {row.map((cardData) => (
+            <div
+              className={
+                cardData.small
+                  ? styles.flipCardContainerLandscapeSmall
+                  : styles.flipCardContainerLandscape
+              }
+              style={{ transform: `rotate(${cardData.rotate}deg)` }}
+            >
+              <FlipCard
+                title="My cool card"
+                delay={cardData.delay}
+                rotate={cardData.rotate}
+                tagRotation={5}
+                backgroundGradients={
+                  rowNumber === 0
+                    ? [gradient1, gradient3]
+                    : [gradient3, gradient5]
+                }
+                variants={cardVariantsLeftToRight}
+              >
+                {cardData.children}
+              </FlipCard>
+            </div>
+          ))}
         </div>
-
-        <div
-          className={styles.flipCardContainerLandscapeSmall}
-          style={{ transform: `rotate(${10}deg)` }}
-        >
-          <FlipCard
-            title="My cool card"
-            delay={0}
-            rotate={10}
-            tagRotation={5}
-            backgroundGradients={[gradient1, gradient3]}
-            variants={cardVariantsLeftToRight}
-          >
-            <p className={styles.contentCardText}>First: Tooling</p>
-          </FlipCard>
-        </div>
-
-        <div
-          className={styles.flipCardContainerLandscape}
-          style={{ transform: `rotate(${10}deg)` }}
-        >
-          <FlipCard
-            title="My cool card"
-            delay={0}
-            rotate={10}
-            tagRotation={5}
-            backgroundGradients={[gradient1, gradient3]}
-            variants={cardVariantsLeftToRight}
-          >
-            <p className={styles.contentCardText}>First: Tooling</p>
-          </FlipCard>
-        </div>
-      </div>
-
-      <div className={styles.row}>
-        <div
-          className={styles.flipCardContainerLandscapeSmall}
-          style={{ transform: `rotate(${10}deg)` }}
-        >
-          <FlipCard
-            title="My cool card"
-            delay={0}
-            rotate={10}
-            tagRotation={5}
-            backgroundGradients={[gradient1, gradient3]}
-            variants={cardVariantsLeftToRight}
-          >
-            <p className={styles.contentCardText}>First: Tooling</p>
-          </FlipCard>
-        </div>
-        <div
-          className={styles.flipCardContainerLandscape}
-          style={{ transform: `rotate(${10}deg)` }}
-        >
-          <FlipCard
-            title="My cool card"
-            delay={0}
-            rotate={10}
-            tagRotation={5}
-            backgroundGradients={[gradient1, gradient3]}
-            variants={cardVariantsLeftToRight}
-          >
-            <p className={styles.contentCardText}>First: Tooling</p>
-          </FlipCard>
-        </div>
-        <div
-          className={styles.flipCardContainerLandscapeSmall}
-          style={{ transform: `rotate(${10}deg)` }}
-        >
-          <FlipCard
-            title="My cool card"
-            delay={0}
-            rotate={10}
-            tagRotation={5}
-            backgroundGradients={[gradient1, gradient3]}
-            variants={cardVariantsLeftToRight}
-          >
-            <p className={styles.contentCardText}>First: Tooling</p>
-          </FlipCard>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
