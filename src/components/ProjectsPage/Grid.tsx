e07e0b5da872e996754751fcf9bb4eeb3988da0f
Jay Pitman <jakeseanp@gmail.com>
2022-08-21 11:05:14 +1000

    Calculate row count based on window dimensions

--- src/components/ProjectsPage/Grid.tsx
+++ src/components/ProjectsPage/Grid.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import useWindowDimensions from "../../shared/useWindowDimensions";
 
 import styles from "./Grid.module.scss";
 
@@ -14,32 +15,36 @@ const columnVariants = {
   imagesAppear: { opacity: 0, transition: { duration: 1 } },
 };
 
-const rowCount = 22;
-const rowHeight = `${100 / rowCount}vh`;
-const colCount = 40;
-const colWidth = `${100 / colCount}vw`;
-
-const rows = new Array(rowCount).fill(
-  <motion.div
-    className={styles.rowBorder}
-    style={{ height: rowHeight, marginBottom: rowHeight }}
-  />
-);
-const columns = new Array(colCount).fill(
-  <motion.div
-    className={styles.columnBorder}
-    style={{ width: colWidth, marginRight: colWidth }}
-  />
-);
+const rows = (rowCount: number, rowHeight: string) =>
+  new Array(rowCount).fill(
+    <motion.div
+      className={styles.rowBorder}
+      style={{ height: rowHeight, marginBottom: rowHeight }}
+    />
+  );
+const columns = (colCount: number, colWidth: string) =>
+  new Array(colCount).fill(
+    <motion.div
+      className={styles.columnBorder}
+      style={{ width: colWidth, marginRight: colWidth }}
+    />
+  );
 
 const Grid = () => {
+  const { width, height } = useWindowDimensions();
+  const ratio = width / height;
+  const colCount = 40;
+  const colWidth = `${100 / colCount}vw`;
+  const rowCount = Math.floor(colCount / ratio);
+  const rowHeight = `${100 / rowCount}vh`;
+
   return (
     <motion.div className={styles.container}>
       <motion.div className={styles.rows} variants={rowVariants}>
-        {rows}
+        {rows(rowCount, rowHeight)}
       </motion.div>
       <motion.div className={styles.columns} variants={columnVariants}>
-        {columns}
+        {columns(colCount, colWidth)}
       </motion.div>
     </motion.div>
   );
