6817c380605e2e1995279831dd2f70a567745ea6
Jake Pitman <jakeseanp@gmail.com>
2022-09-04 12:04:22 +1000

    Extract HightlightedProject & remove scrollbar

---
+++ src/components/ProjectsPage/ProjectHighlight.tsx
@@ -0,0 +1,68 @@
+import React, { useContext } from "react";
+import { motion, useAnimation } from "framer-motion";
+
+import styles from "./ProjectHighlight.module.scss";
+import BlueprintShape from "./BlueprintShape";
+import testBrown from "./pictures/test-brown-phone.png";
+import listBlue from "./pictures/list-blue-phone.png";
+import Grid from "./Grid";
+import TestAppBlueprint from "./TestAppBlueprint";
+import ListAppBlueprint from "./ListAppBlueprint";
+import BrowserContext from "../../shared/browserContext";
+
+const imageVariants = {
+  hidden: { opacity: 0 },
+  imagesAppear: { opacity: 1, transition: { duration: 1 } },
+};
+
+const ProjectsPage = () => {
+  const controls = useAnimation();
+  const { isLandscape } = useContext(BrowserContext);
+
+  const openingSequence = async () => {
+    await controls.start("draw");
+    await controls.start("imagesAppear");
+  };
+
+  return (
+    <motion.div
+      className={styles.pageContainer}
+      animate={controls}
+      initial="hidden"
+      onViewportEnter={() => openingSequence()}
+    >
+      <Grid />
+      <div className={styles.appImageContainer}>
+        <TestAppBlueprint />
+        <motion.img
+          src={testBrown}
+          alt=""
+          className={styles.image}
+          variants={imageVariants}
+        />
+      </div>
+      {isLandscape && (
+        <div className={styles.appImageContainer}>
+          <ListAppBlueprint />
+          <motion.img
+            src={listBlue}
+            alt=""
+            className={styles.image}
+            variants={imageVariants}
+          />
+        </div>
+      )}
+      {/* <motion.div className={styles.shapeContainer}>
+        <BlueprintShape shape="square" color="white" />
+        <motion.img
+          src={listing}
+          alt=""
+          className={styles.image}
+          variants={imageVariants}
+        />
+      </motion.div> */}
+    </motion.div>
+  );
+};
+
+export default ProjectsPage;
--- src/components/ProjectsPage/ProjectsPage.tsx
+++ src/components/ProjectsPage/ProjectsPage.tsx
@@ -1,67 +1,13 @@
-import React, { useContext } from "react";
-import { motion, useAnimation } from "framer-motion";
+import React from "react";
 
+import ProjectHighlight from "./ProjectHighlight";
 import styles from "./ProjectsPage.module.scss";
-import BlueprintShape from "./BlueprintShape";
-import testBrown from "./pictures/test-brown-phone.png";
-import listBlue from "./pictures/list-blue-phone.png";
-import Grid from "./Grid";
-import TestAppBlueprint from "./TestAppBlueprint";
-import ListAppBlueprint from "./ListAppBlueprint";
-import BrowserContext from "../../shared/browserContext";
-
-const imageVariants = {
-  hidden: { opacity: 0 },
-  imagesAppear: { opacity: 1, transition: { duration: 1 } },
-};
 
 const ProjectsPage = () => {
-  const controls = useAnimation();
-  const { isLandscape } = useContext(BrowserContext);
-
-  const openingSequence = async () => {
-    await controls.start("draw");
-    await controls.start("imagesAppear");
-  };
-
   return (
-    <motion.div
-      className={styles.pageContainer}
-      animate={controls}
-      initial="hidden"
-      onViewportEnter={() => openingSequence()}
-    >
-      <Grid />
-      <div className={styles.appImageContainer}>
-        <TestAppBlueprint />
-        <motion.img
-          src={testBrown}
-          alt=""
-          className={styles.image}
-          variants={imageVariants}
-        />
-      </div>
-      {isLandscape && (
-        <div className={styles.appImageContainer}>
-          <ListAppBlueprint />
-          <motion.img
-            src={listBlue}
-            alt=""
-            className={styles.image}
-            variants={imageVariants}
-          />
-        </div>
-      )}
-      {/* <motion.div className={styles.shapeContainer}>
-        <BlueprintShape shape="square" color="white" />
-        <motion.img
-          src={listing}
-          alt=""
-          className={styles.image}
-          variants={imageVariants}
-        />
-      </motion.div> */}
-    </motion.div>
+    <div className={styles.pageContainer}>
+      <ProjectHighlight />;
+    </div>
   );
 };
 
