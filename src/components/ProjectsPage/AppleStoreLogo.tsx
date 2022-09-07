f130b589dfdad64b81e1f2bf200de7209dd4b53e
Jay Pitman <jakeseanp@gmail.com>
2022-09-07 23:38:00 +1000

    Turn button into a tag

--- src/components/ProjectsPage/AppleStoreLogo.tsx
+++ src/components/ProjectsPage/AppleStoreLogo.tsx
@@ -4,13 +4,18 @@ import { BsApple } from "react-icons/bs";
 
 const AppleStoreLogo = () => {
   return (
-    <div className={styles.container}>
+    <a
+      href="https://apps.apple.com/us/app/chess-openings-trainer/id1606980198"
+      rel="noreferrer"
+      target="_blank"
+      className={styles.container}
+    >
       <BsApple color="black" size="50px" />
       <div className={styles.textContainer}>
         <p className={styles.upperText}>Download on the</p>
         <p className={styles.lowerText}>App Store</p>
       </div>
-    </div>
+    </a>
   );
 };
 
