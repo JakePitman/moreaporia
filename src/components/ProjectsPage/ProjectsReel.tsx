edfd03a4a47cfe6ba656a6dc5a9f1b5f1c25eabe
Jake Pitman <jakeseanp@gmail.com>
2022-10-17 13:46:33 +1000

    Add basic animated ProjectPortrait cards

--- src/components/ProjectsPage/ProjectsReel.tsx
+++ src/components/ProjectsPage/ProjectsReel.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { motion } from "framer-motion";
 
 import Project from "./Project";
+import ProjectPortrait from "./ProjectPortrait";
 import styles from "./ProjectsReel.module.scss";
 import PROJECT_REEL_DATA from "./ProjectReelData";
+import BrowserContext from "../../shared/browserContext";
 
 const ProjectsReel = () => {
+  const { isLandscape } = useContext(BrowserContext);
   return (
     <motion.div className={styles.pageContainer}>
-      {PROJECT_REEL_DATA.map((data) => (
-        <Project {...data} />
-      ))}
+      {isLandscape
+        ? PROJECT_REEL_DATA.map((data) => <Project {...data} />)
+        : PROJECT_REEL_DATA.map((data) => <ProjectPortrait {...data} />)}
     </motion.div>
   );
 };
