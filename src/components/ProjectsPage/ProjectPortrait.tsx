10ca93262a5d73f428b32a4f822ae52d9f41c456
Jake Pitman <jakeseanp@gmail.com>
2022-10-18 11:16:04 +1000

    Animate ProjectPortrait container

--- src/components/ProjectsPage/ProjectPortrait.tsx
+++ src/components/ProjectsPage/ProjectPortrait.tsx
@@ -10,6 +10,27 @@ const imageVariants = {
   hidden: { opacity: 0 },
   renderImage: { opacity: 1 },
 };
+// const textSectionTopVariants = {
+//   hidden: { left: -400 },
+//   drawSVG: {
+//     left: 0,
+//     transition: { duration: 0.4, type: "spring", bounce: 0.2 },
+//   },
+// };
+// const textSectionBottomVariants = {
+//   hidden: { left: 400 },
+//   drawSVG: {
+//     left: 0,
+//     transition: { duration: 0.4, type: "spring", bounce: 0.2 },
+//   },
+// };
+const textSectionVariants = ({ leftOffset }: { leftOffset: number }) => ({
+  hidden: { left: leftOffset },
+  drawSVG: {
+    left: 0,
+    transition: { duration: 0.4, type: "spring", bounce: 0.2 },
+  },
+});
 
 const ProjectPortrait = ({
   title,
@@ -36,9 +57,14 @@ const ProjectPortrait = ({
       viewport={{ once: true, margin: "-30%" }}
       initial="hidden"
     >
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.subtext}>{year}</p>
-      <motion.div className={styles.pictureContainer}>
+      <motion.div
+        className={styles.textSectionTop}
+        variants={textSectionVariants({ leftOffset: -400 })}
+      >
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.subtext}>{year}</p>
+      </motion.div>
+      <div className={styles.pictureContainer}>
         <motion.img
           variants={imageVariants}
           className={styles.picture}
@@ -46,32 +72,37 @@ const ProjectPortrait = ({
           alt={imgAlt}
         />
         <Blueprint />
-      </motion.div>
-      <p className={classNames(styles.subtext, styles.textAlignRight)}>
-        {tools.map(({ name, href }, i) => (
-          <React.Fragment key={i}>
-            {`${name}${i < tools.length - 1 ? "·" : ""}`}
-          </React.Fragment>
-        ))}
-      </p>
-      <p
-        className={classNames(
-          styles.subtext,
-          styles.textAlignRight,
-          styles.topSpacing
-        )}
+      </div>
+      <motion.div
+        className={styles.textSectionBottom}
+        variants={textSectionVariants({ leftOffset: 400 })}
       >
-        {links.map(({ Icon, href }, i) => (
-          <React.Fragment key={i}>
-            <a href={href} target="_blank" rel="noreferrer">
-              <Icon color={colors.primaryBackground} size={25} />
-            </a>
-            {i < links.length - 1 ? (
-              <p className={styles.toolSeparator}>·</p>
-            ) : null}
-          </React.Fragment>
-        ))}
-      </p>
+        <p className={classNames(styles.subtext, styles.textAlignRight)}>
+          {tools.map(({ name, href }, i) => (
+            <React.Fragment key={i}>
+              {`${name}${i < tools.length - 1 ? "·" : ""}`}
+            </React.Fragment>
+          ))}
+        </p>
+        <p
+          className={classNames(
+            styles.subtext,
+            styles.textAlignRight,
+            styles.topSpacing
+          )}
+        >
+          {links.map(({ Icon, href }, i) => (
+            <React.Fragment key={i}>
+              <a href={href} target="_blank" rel="noreferrer">
+                <Icon color={colors.primaryBackground} size={25} />
+              </a>
+              {i < links.length - 1 ? (
+                <p className={styles.toolSeparator}>·</p>
+              ) : null}
+            </React.Fragment>
+          ))}
+        </p>
+      </motion.div>
     </motion.div>
   );
 };
