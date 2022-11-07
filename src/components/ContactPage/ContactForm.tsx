ca2624523a98944d548015219ce6a38a01580c51
jakepitman <jakeseanp@gmail.com>
2022-11-07 22:21:54 +0900

    Add basic styles to contact form

--- src/components/ContactPage/ContactForm.tsx
+++ src/components/ContactPage/ContactForm.tsx
@@ -1,26 +1,57 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import styles from "./ContactForm.module.scss";
 
+const variants = {
+  hidden: {
+    opacity: 0,
+  },
+  splineStarted: {
+    opacity: 1,
+  },
+};
+
 const ContactForm = () => {
   return (
-    <div className={styles.container}>
+    <motion.div className={styles.container} variants={variants}>
       <form
         className={styles.form}
         action="https://formspree.io/f/xdojpqba"
         method="POST"
       >
+        <h2 className={styles.heading}>Contact me</h2>
         <label>
-          Your email:
-          <input type="email" name="email" />
+          <input
+            className={styles.inputField}
+            type="username"
+            name="username"
+            placeholder="Your name..."
+          />
         </label>
         <label>
-          Your message:
-          <textarea name="message"></textarea>
+          <input
+            className={styles.inputField}
+            type="email"
+            name="email"
+            placeholder="Your email..."
+          />
         </label>
-        <button type="submit">Send</button>
+        <label>
+          <textarea
+            className={styles.textArea}
+            name="message"
+            placeholder="Your message..."
+          ></textarea>
+        </label>
+        <div className={styles.submitButtonContainer}>
+          <div className={styles.submitButtonBackground} />
+          <button className={styles.submitButton} type="submit">
+            Send
+          </button>
+        </div>
       </form>
-    </div>
+    </motion.div>
   );
 };
 
